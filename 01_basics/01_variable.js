470a1da1deedddbf587a7867c08609a332c88bb2
ritik-only <ritikjangra000@gmail.com>
2025-02-21 13:14:38 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "as@gmail.com"
+var accountPassword = "785643"
+accountCity = "delhi"
+let accountState 
+
+// accountId = 234564   // not allowed 
+ 
+accountEmail = "def@gamil.com"
+accountPassword = "765434"
+accountCity = "jind"
+
+console.log("accountId");
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);

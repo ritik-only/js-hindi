6f536c30c4b526a1b264248a4494ccaee30177b1
ritik-only <ritikjangra000@gmail.com>
2025-03-05 12:45:49 +0000

    reduce2

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,9 +1,37 @@
+//reduce
+
 const myNums = [1,2,3,4]
  
-// const Total = myNums.reduce(function(acc,currval){   //acc = accurate
-//     console.log(`the ${acc} and ${currval}`);     //currval = current value
-//     return acc+ currval
+const Total = myNums.reduce(function(acc,currval){   //acc = accurate
+    console.log(`the ${acc} and ${currval}`);     //currval = current value
+    return acc+ currval
     
-// },0)  // here 0 is accurate value 
+},0)  // here 0 is accurate value 
+
 const tell = myNums.reduce((acc,currval) => acc +currval,7)
+console.log(Total);
 console.log(tell);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "  py course",
+        price: 999
+    },
+    {
+        itemName: "  mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "  data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.itemName, 0)
+
+console.log(priceToPay);
